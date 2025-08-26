ded658ef27c15f010b0d6f18944bf4d1f9ae5875
Guufla <liamabramov@gmail.com>
2025-08-25 20:06:02 -0400

    #2 Working on drag for character movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] float moveSpeed;
 
+    [SerializeField] float groundDrag;
+
+    [Header("Ground Check")]
+    [SerializeField] LayerMask groundLayer;
+
     [SerializeField] Transform orientation;
+
+    [SerializeField] float playerHeight;
+
+    bool isOnGround;
+
+    PlayerInput input;
 
     float horizontalInput;
     float verticalInput;
@@ -16,53 +29,52 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        input = GetComponent<PlayerInput>();
 
+        input.actions["Move"].performed += OnMove;
+        input.actions["Move"].canceled += OnStop;
+        
+        rb.freezeRotation = true; 
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
+        isOnGround = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
 
+        if (isOnGround)
+        {
+            rb.linearDamping = groundDrag;
+        }
+        else
+        {
+            rb.linearDamping = 0f;
+        }
+    }
+
+    
+    void FixedUpdate()
+    {
+        MovePlayer();
     }
 
     void MovePlayer()
     {
-        
-    }
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-    void forwardHeld()
-    {
-        horizontalInput = 1;
+        rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
     }
-    void forwardReleased()
+    void OnMove(InputAction.CallbackContext context)
+    {
+        horizontalInput = context.ReadValue<Vector2>().x;
+        verticalInput = context.ReadValue<Vector2>().y;
+    }
+    void OnStop(InputAction.CallbackContext context)
     {
         horizontalInput = 0;
-    }
-
-    void backwardsHeld()
-    {
-        horizontalInput = -1;
-    }
-    void backwardsReleased()
-    {
-        horizontalInput = 0;
-    }
-
-    void rightHeld()
-    {
-        verticalInput = -1;
-    }
-    void rightReleased()
-    {
         verticalInput = 0;
     }
 
-    void leftHeld()
-    {
-        verticalInput = -1;
-    }
-    void leftReleased()
-    {
-        verticalInput = 0;
-    }
+
 }
